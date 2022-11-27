53dd7b27ee214e59f4dfd8069fa9a86c4fb3df8e
creasedigital <ojorechris@gmail.com>
2022-11-27 09:38:50 +0100

    color palette added

---
+++ constants/Color.js
@@ -0,0 +1,24 @@
+const Colors = {
+	black: "#000000",
+	white: "#FFFFFF",
+	lightRed: "#fecaca",
+	darkRed: "#dc2626",
+	lime: "#a3e635",
+	caution: "#facc15",
+	lightGreen: "#dcfce7",
+	secondaryGreen: "#22c55e",
+	darkGreen: "#14532d",
+	gray0: "#fafafa",
+	gray50: "#f4f4f5",
+	gray100: "#e4e4e7",
+	gray200: "#e4e4e7",
+	gray300: "#d4d4d8",
+	gray400: "#9ca3af",
+	gray500: "#71717a",
+	gray600: "#52525b",
+	gray700: "#3f3f46",
+	gray800: "#27272a",
+	gray900: "#18181b",
+};
+
+export default Colors;
